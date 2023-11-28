f357ee11140338a360e4f52f4db82bc1ffb9c23b
Jefrey Zavala <jefreyzavala1@gmail.com>
2023-11-28 00:21:51 -0500

    react app

---
+++ src/useBreedList.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import fetchBreedList from "./fetchBreedList";
+import { useQuery } from "@tanstack/react-query";
+const localCache = {};
+
+export default function useBreedList(animal) {
+  const results = useQuery(["breeds", animal], fetchBreedList);
+
+  return [results?.data?.breeds ?? [], results.status];
+}
